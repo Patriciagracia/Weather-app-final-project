b5ca7d034a2ff3ef58a0550da4dba71c26fd6646
Patriciagracia <patriciagraciaherrero@gmail.com>
2023-10-27 15:40:53 +0200

    Introduced axios library

---
+++ index.js
@@ -0,0 +1,9 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+let apiKey = "e4167474503t4a0o133bbfcc9fa69a38";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
