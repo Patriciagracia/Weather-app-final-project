e28e68faedcef5cd9dae77844b59870e125e4100
Patriciagracia <patriciagraciaherrero@gmail.com>
2023-10-30 16:10:21 +0100

    Introduced current weather icon

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+let apiKey = "e4167474503t4a0o133bbfcc9fa69a38";
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
-let apiKey = "e4167474503t4a0o133bbfcc9fa69a38";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+
+function currentWeatherIcon(response) {
+  let iconElement = document.querySelector("#weatherIcon");
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+}
+
+axios.get(apiUrl).then(currentWeatherIcon);
